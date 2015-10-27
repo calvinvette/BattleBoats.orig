8a073c1e5db877858c9682aea2a15636d2ffc2e3
Student <calvin@dragontale.com>
2015-10-27 11:31:47 -0400

    PlayerJPADAO Stub

---
+++ src/com/battleboats/data/PlayerJPADAO.java
@@ -0,0 +1,32 @@
+package com.battleboats.data;
+
+import java.util.List;
+
+public class PlayerJPADAO {
+	
+	public Player insert(Player player) {
+		return null;
+	}
+	
+	public Player update(Player player) {
+		return null;
+	}
+	
+	public Player delete(Player player) {
+		return null;
+	}
+	
+	public Player findById(Integer id) {
+		return null;
+	}
+
+	public Player findByUserName(String userName) {
+		return null;
+	}
+	
+	public List<Player> findByEmail(String email) {
+		return null;
+	}
+	
+	
+}
