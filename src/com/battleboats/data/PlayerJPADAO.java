5418706448f2984a4a3f8e12e351213a6fc89007
Student <calvin@dragontale.com>
2015-10-27 11:45:32 -0400

    Partial PlayerDAO

--- src/com/battleboats/data/PlayerJPADAO.java
+++ src/com/battleboats/data/PlayerJPADAO.java
@@ -2,22 +2,40 @@ package com.battleboats.data;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 public class PlayerJPADAO {
+	private EntityManager entityManager;
 	
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
 	public Player insert(Player player) {
-		return null;
+		getEntityManager().persist(player); // INSERT INTO PLAYER, Gen ID
+		return player;
 	}
 	
 	public Player update(Player player) {
-		return null;
+		// Transient Objects: new or recently DB-deleted
+		// Managed: auto-update
+		// Detached: EM that found object earlier no longer in memory
+		// merge() pushes a detached object back in to "managed" mode
+		// forces updates to happen
+		return getEntityManager().merge(player);
 	}
 	
 	public Player delete(Player player) {
-		return null;
+		getEntityManager().remove(player); // DELETE FROM PLAYER WHERE...
+		return player; // no longer valid player in DB
 	}
 	
 	public Player findById(Integer id) {
-		return null;
+		return getEntityManager().find(Player.class, id);
 	}
 
 	public Player findByUserName(String userName) {
